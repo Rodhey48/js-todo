d9305dc42c64d498402cf87f0269c9d9f52ef614
RehanAjiNarwindo <rhnajin@gmail.com>
2023-11-28 10:52:08 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let angka;
+let palindrome = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99,
+];
+
+for (let angka = 35; angka <= 100; angka++) {
+  if (palindrome.includes(angka)) {
+    console.log(angka + " adalah palindrome");
+    break;
+  }
+}
